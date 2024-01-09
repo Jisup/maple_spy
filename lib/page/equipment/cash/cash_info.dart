07461d0eabae8dd28b038798937430d02cc38258
jisup <xrl0603@naver.com>
2024-01-09 20:16:05 +0900

    예외 처리 작업 진행 (데이터)
    캐시 장비 관련 수정
    - 추후 고도화를 위한 기반 마련
    - Icon이 존재하는 경우에만 이미지를 설정할 수 있도록 예외 추가

--- lib/page/equipment/cash/cash_info.dart
+++ lib/page/equipment/cash/cash_info.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maplespy/config/const_config.dart';
 import 'package:maplespy/model/equipment/cash_item_model.dart';
-import 'package:maplespy/widget/equipment/equipment_slot_widget.dart';
 
 class CashInfo extends ConsumerWidget {
   const CashInfo({super.key, required this.name, required this.cashItem});
@@ -11,11 +12,47 @@ class CashInfo extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return cashItem.cashItemName != null
-        ? EquipmentSlotWidget(
-            name: name,
-            imageUrl: cashItem.cashItemIcon,
-          )
-        : EquipmentSlotWidget(name: name);
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      margin: EdgeInsets.all(DimenConfig.minDimen),
+      padding: EdgeInsets.all(DimenConfig.subDimen),
+      decoration: BoxDecoration(
+          color: colorScheme.onSecondary,
+          borderRadius: BorderRadius.circular(RadiusConfig.subRadius),
+          boxShadow: [
+            BoxShadow(
+                blurRadius: RadiusConfig.subRadius,
+                offset: Offset(-3, -3),
+                color: Colors.white,
+                inset: true),
+            BoxShadow(
+                blurRadius: RadiusConfig.subRadius,
+                offset: Offset(3, 3),
+                color: Colors.black87,
+                inset: true)
+          ]),
+      child: Stack(
+        children: [
+          Text(
+            name,
+            style: TextStyle(
+                fontSize: FontConfig.minSize,
+                letterSpacing: SpacingConfig.minSpacing,
+                color: Colors.white70,
+                fontWeight: FontWeight.bold),
+          ),
+          cashItem.cashItemIcon != null
+              ? SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Image(
+                    image: NetworkImage(cashItem.cashItemIcon!),
+                    fit: BoxFit.contain,
+                  ),
+                )
+              : SizedBox.shrink(),
+        ],
+      ),
+    );
   }
 }

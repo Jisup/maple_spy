9f46a1353ee7e36c319b955bbae181a5fe976c0f
jisup <xrl0603@naver.com>
2024-01-11 18:14:50 +0900

    예외 처리 작업 진행 (데이터)
    닉네임 입력 시 지정된 문자 이외 입력 방지
    - 숫자/영대소문자/한글자음/한글
    - ※ 한글자음 입력을 방지할 경우 한글을 입력할 수 없음

--- lib/page/main_home_page.dart
+++ lib/page/main_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:maplespy/config/const_config.dart';
@@ -84,6 +85,15 @@ class _MainHomeState extends ConsumerState<MainHomePage> {
                                 border: OutlineInputBorder(),
                                 labelText: '닉네임',
                               ),
+                              inputFormatters: [
+                                TextInputFormatter.withFunction(
+                                    (oldValue, newValue) {
+                                  return newValue.text.contains(
+                                          RegExp(r'[^0-9a-zA-Zㄱ-ㅎ가-힣]'))
+                                      ? oldValue
+                                      : newValue;
+                                })
+                              ],
                               validator: (value) {
                                 if (value == null || value.isEmpty) {
                                   return '닉네임을 입력해주세요.';

13c2b46fca3ddc7439803ca7501785c7ada518b0
jisup <xrl0603@naver.com>
2024-03-22 18:24:01 +0900

    스탯 데이터 전처리 조건 추가
    null값을 지니는 경우를 판단

--- lib/model/stat/stat_model.dart
+++ lib/model/stat/stat_model.dart
@@ -47,45 +47,49 @@ class FinalStat {
     statName = json['stat_name'];
     statValue = json['stat_value'];
 
-    switch (statName) {
-      case '최소 스탯공격력':
-      case '최대 스탯공격력':
-      case '전투력':
-        var value =
-            subFormatter.format(int.parse(statValue!)).toString().split(',');
-        var len = measure.length - value.length;
-        statValue = '';
-        for (var i = 0; i < value.length; i++) {
-          statValue =
-              '${statValue!}${i != 0 ? ' ' : ''}${value[i]}${measure[len + i]}';
-        }
-        break;
-      case '데미지':
-      case '보스 몬스터 데미지':
-      case '최종 데미지':
-      case '방어율 무시':
-      case '크리티컬 확률':
-      case '크리티컬 데미지':
-      case '아이템 드롭률':
-      case '메소 획득량':
-      case '버프 지속시간':
-      case '일반 몬스터 데미지':
-      case '재사용 대기시간 감소 (%)':
-      case '재사용 대기시간 미적용':
-      case '속성 내성 무시':
-      case '상태이상 추가 데미지':
-      case '추가 경험치 획득':
-      case '이동속도':
-      case '점프력':
-        statValue = '${statValue!}%';
-        break;
-      case '재사용 대기시간 감소 (초)':
-        statValue = '${statValue}초';
-      case '공격 속도':
-        statValue = '${statValue}단계';
-        break;
-      default:
-        statValue = mainFormatter.format(int.parse(statValue!)).toString();
+    if (statValue == null) {
+      statValue = '';
+    } else {
+      switch (statName) {
+        case '최소 스탯공격력':
+        case '최대 스탯공격력':
+        case '전투력':
+          var value =
+              subFormatter.format(int.parse(statValue!)).toString().split(',');
+          var len = measure.length - value.length;
+          statValue = '';
+          for (var i = 0; i < value.length; i++) {
+            statValue =
+                '${statValue!}${i != 0 ? ' ' : ''}${value[i]}${measure[len + i]}';
+          }
+          break;
+        case '데미지':
+        case '보스 몬스터 데미지':
+        case '최종 데미지':
+        case '방어율 무시':
+        case '크리티컬 확률':
+        case '크리티컬 데미지':
+        case '아이템 드롭률':
+        case '메소 획득량':
+        case '버프 지속시간':
+        case '일반 몬스터 데미지':
+        case '재사용 대기시간 감소 (%)':
+        case '재사용 대기시간 미적용':
+        case '속성 내성 무시':
+        case '상태이상 추가 데미지':
+        case '추가 경험치 획득':
+        case '이동속도':
+        case '점프력':
+          statValue = '${statValue!}%';
+          break;
+        case '재사용 대기시간 감소 (초)':
+          statValue = '${statValue}초';
+        case '공격 속도':
+          statValue = '${statValue}단계';
+          break;
+        default:
+          statValue = mainFormatter.format(int.parse(statValue!)).toString();
+      }
     }
   }
 
